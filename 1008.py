a9fc72604acea805b9d59d3b5b2f076841ba1a88
Michel Melo <michel.ferreira.melo@gmail.com>
2020-05-31 22:00:50 -0300

    Salário

---
+++ 1008.py
@@ -0,0 +1,13 @@
+'''
+URI Online Judge | 1008
+autor: Michel Melo
+'''
+
+num_funcionario = int(input())
+num_horas = int(input())
+valor_hora = float(input())
+
+salario = num_horas * valor_hora
+
+print("NUMBER = {}".format(num_funcionario))
+print("SALARY = U$ {:.2f}".format(salario))
