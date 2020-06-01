5eb14922fff6124fc284c4dc48c8be57736e33c3
Michel Melo <michel.ferreira.melo@gmail.com>
2020-05-31 21:37:28 -0300

    Média 2

---
+++ 1006.py
@@ -0,0 +1,12 @@
+'''
+URI Online Judge | 1006
+autor: Michel Melo
+'''
+
+A = float(input())
+B = float(input())
+C = float(input())
+
+media = (A*2 + B*3 + C*5) / 10
+
+print("MEDIA = {:.1f}".format(media))
