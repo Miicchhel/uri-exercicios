0540d260e3aa9fcfb1568dd65f98e823bb10ce1e
Michel Melo <michel.ferreira.melo@gmail.com>
2020-05-31 22:00:13 -0300

    Diferença

---
+++ 1007.py
@@ -0,0 +1,13 @@
+'''
+URI Online Judge | 1007
+autor: Michel Melo
+'''
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+diferenca = (A*B - C*D)
+
+print("DIFERENCA = {}".format(diferenca))
